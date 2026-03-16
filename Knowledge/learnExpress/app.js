d134d541e25f0f4cd9e0f5d75afa076141c2a21b
SyedSaif07 <syed.sky07@gmail.com>
2026-03-16 17:38:53 +0300

    routing - get post use

--- Knowledge/learnExpress/app.js
+++ Knowledge/learnExpress/app.js
@@ -1,13 +1,32 @@
 //  Core Modules
-const http = require("http");
+// const http = require("http");
 
 // External Modules
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+// Order of usage of middleware is very important
+app.get("/",(req, res, next) => {
+  console.log("First middleware", req.url, req.method);
+  // res.send("<p>Welcome to my first page </p>")
+  next(); // cannot call next after send
+});
+
+
+app.post("/submit-details", (req, res, next) => {
+  console.log("Second middleware", req.url, req.method);
+  res.send("<p>Welcome to my third page </p>")
+});
+
+app.use("/",(req, res, next) => {
+  console.log("First middleware", req.url, req.method);
+  res.send("<p>Welcome to my second page </p>") // send means response ends
+  next(); // cannot call next after send
+}); 
+
+// const server = http.createServer(app);
 const PORT = 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost: ${PORT}`);
 });
