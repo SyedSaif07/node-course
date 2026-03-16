d411a9a2ff348693a7c989d803d26593136b13d7
SyedSaif07 <syed.sky07@gmail.com>
2026-03-16 15:32:06 +0300

    implement express

---
+++ Knowledge/learnExpress/app.js
@@ -0,0 +1,13 @@
+//  Core Modules
+const http = require("http");
+
+// External Modules
+const express = require('express')
+
+const app = express();
+
+const server = http.createServer(app);
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost: ${PORT}`);
+});
