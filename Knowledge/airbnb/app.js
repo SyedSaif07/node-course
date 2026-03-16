b890738a314f4a16f2ea5728cebc8f93456a84d8
SyedSaif07 <syed.sky07@gmail.com>
2026-03-16 22:44:26 +0300

    path for html files

--- Knowledge/airbnb/app.js
+++ Knowledge/airbnb/app.js
@@ -1,3 +1,6 @@
+// Core Module
+const path = require('path')
+
 // External Module
 const express = require("express");
 
@@ -13,10 +16,13 @@ app.use((req, res, next) => {
 });
 
 app.use(express.urlencoded());
-app.use(userRouter);
-app.use(hostRouter);
+app.use("/", userRouter);
+app.use("/host", hostRouter);
 
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname, "views", '404.html'));
 
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
