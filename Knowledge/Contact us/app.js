665c1f50e487be1b697d1500b1d8d7fd1a462b2c
SyedSaif07 <syed.sky07@gmail.com>
2026-03-16 17:57:17 +0300

    sample cotact us routing

---
+++ Knowledge/Contact us/app.js
@@ -0,0 +1,34 @@
+// External Modules
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next)=> {
+    console.log("First dummy middleware", req.url, req.method)
+    next();
+})
+
+app.use((req, res, next)=> {
+    console.log("Second dummy middleware", req.url, req.method)
+    next();
+})
+
+// app.use((req, res, next)=> {
+//     console.log("Third dummy middleware", req.url, req.method)
+//     res.send("<h1>Welcome to third</h1>");
+// })
+
+app.get("/", (req, res, next)=> {
+    console.log("Handling for / GET", req.url, req.method)
+   res.send(`<h1>Handling for / GET</h1>`);
+})
+
+app.get("/contact-us", (req, res, next)=> {
+    console.log("Handling for / GET", req.url, req.method)
+   res.send(`<h1>Please provide your details </h1>`);
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost: ${PORT}`);
+});
